ea9da4b8116aa2d66a58b974b10bd824a3132b6d
sauravk2605 <saurav.kumar@tamr.com>
2023-06-13 14:57:30 +0530

    fix: pojo

---
+++ src/main/java/pojo/BookingPayload.java
@@ -0,0 +1,64 @@
+package pojo;
+
+public class BookingPayload {
+    private String firstname;
+    private String lastname;
+
+    private int totalprice;
+
+    private BookingDates bookingdates;
+
+    private boolean depositpaid;
+
+    private String additionalneeds;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getTotalprice() {
+        return totalprice;
+    }
+
+    public void setTotalprice(int totalprice) {
+        this.totalprice = totalprice;
+    }
+
+    public BookingDates getBookingdates() {
+        return bookingdates;
+    }
+
+    public void setBookingdates(BookingDates bookingdates) {
+        this.bookingdates = bookingdates;
+    }
+
+    public boolean isDepositpaid() {
+        return depositpaid;
+    }
+
+    public void setDepositpaid(boolean depositpaid) {
+        this.depositpaid = depositpaid;
+    }
+
+    public String getAdditionalneeds() {
+        return additionalneeds;
+    }
+
+    public void setAdditionalneeds(String additionalneeds) {
+        this.additionalneeds = additionalneeds;
+    }
+
+
+}
